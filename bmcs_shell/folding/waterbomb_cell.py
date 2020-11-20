578114263c917c80fd410c4f7f9c97cb0d9534a8
rch <rostislav.chudoba@rwth-aachen.de>
2020-11-20 10:14:26 +0100

    initial version

---
+++ bmcs_shell/folding/waterbomb_cell.py
@@ -0,0 +1,130 @@
+"""
+
+"""
+from mayavi import mlab
+mlab.init_notebook()
+import bmcs_utils.api as bu
+import sympy as sp
+
+class WBElemSymb(bu.SymbExpr):
+
+    a, b, c = sp.symbols('a, b, c', positive=True)
+    u_2, u_3 = sp.symbols('u_2, u_3', positive=True)
+    alpha = sp.symbols('alpha', nonnepositivegative=True)
+
+    U_0 = sp.Matrix([a, b, 0])
+    W_0 = sp.Matrix([c, 0, 0])
+
+    U = sp.Matrix([a, u_2, u_3])
+    W = sp.Matrix([c * sp.cos(alpha), 0, c * sp.sin(alpha)])
+
+    UW_0 = U_0 - W_0
+
+    L2_U0 = U_0.T * U_0
+    L2_UW0 = UW_0.T * UW_0
+
+    UW = U - W
+
+    L2_U = U.T * U
+    L2_UW = UW.T * UW
+
+    eq_U = sp.Eq(L2_U[0], L2_U0[0])
+    eq_UW = sp.Eq(L2_UW[0], L2_UW0[0])
+
+    u_2_, u_3_ = sp.solve({eq_U, eq_UW}, [u_2, u_3])
+
+    u_2 = u_2_[1]
+    u_3 = u_3_[1]
+    # u_2 = u_2_[0]
+    # u_3 = u_3_[0]
+
+    symb_model_params = ['a', 'b', 'c']
+    symb_expressions = [
+        ('u_2', ('alpha',)),
+        ('u_3', ('alpha',))
+    ]
+
+
+# In[22]:
+
+
+import traits.api as tr
+import numpy as np
+import matplotlib.pylab as plt
+
+class WBElem(bu.InteractiveModel,bu.InjectSymbExpr):
+    name = 'Waterbomb cell'
+    symb_class = WBElemSymb
+
+    a = bu.Float(1, GEO=True)
+    b = bu.Float(1, GEO=True)
+    c = bu.Float(1, GEO=True)
+
+    alpha = bu.Float(1e-5, GEO=True)
+
+    ipw_view = bu.View(
+        bu.Item('alpha', latex = r'\alpha', editor=bu.FloatRangeEditor(low=1e-10,high=np.pi/2)),
+        bu.Item('a'),
+        bu.Item('b'),
+        bu.Item('c'),
+    )
+
+    X_Ia = tr.Property(depends_on='+GEO')
+    '''Array with nodal coordinates I - node, a - dimension
+    '''
+    @tr.cached_property
+    def _get_X_Ia(self):
+        alpha = self.alpha
+        u_2 = self.symb.get_u_2(alpha)
+        u_3 = self.symb.get_u_3(alpha)
+        return np.array([
+            [0,0,0], # 0 point
+            [self.a, u_2, u_3], #U++
+            [-self.a,u_2, u_3], #U-+
+            [self.a,-u_2, u_3], #U+=
+            [-self.a,-u_2, u_3], #U--
+            [self.c * np.cos(alpha), 0, self.c * np.sin(alpha)], # W0+
+            [-self.c * np.cos(alpha), 0, self.c * np.sin(alpha)] # W0-
+            ], dtype=np.float_
+        )
+
+    I_Fi = tr.Property
+    '''Mapping '''
+    @tr.cached_property
+    def _get_I_Fi(self):
+        return np.array([[0,1,2],
+                         [0,3,4],
+                         [0,1,5],
+                         [0,2,6],
+                         [0,3,5],
+                         [0,4,6],
+                         ])
+
+    I_Li = tr.Property
+    '''Mapping '''
+    @tr.cached_property
+    def _get_I_Li(self):
+        return np.array([[0,1],
+                         [1,2],
+                         [2,3],
+                         [3,0],
+                         [2,0]])
+
+    def subplots(self,fig):
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        return ax
+
+    def update_plot(self, axes):
+        ax = axes
+        x, y, z = self.X_Ia.T
+        triangles = self.I_Fi
+        ax.plot_trisurf(x, y, z, triangles=triangles, cmap=plt.cm.Spectral)
+        ax.set_zlim(0.01, np.max([self.a, self.b, self.c]))
+        # r = 1.5
+        # x_ = np.max([r * self.a, r * self.c])
+        # ax.set_xlim(-x_, +x_ )
+        # y_ = r * self.b
+        # ax.set_ylim(-y_, +y_)
+
+
+
