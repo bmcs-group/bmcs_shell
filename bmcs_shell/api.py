89074c5be403df752b9556edb275ae58da55c0ae
ishomam <ishomam@gmail.com>
2024-03-28 16:42:41 +0100

    Version 0.0.8 released to PyPi

--- bmcs_shell/api.py
+++ bmcs_shell/api.py
@@ -1,11 +1,15 @@
-from bmcs_shell.folding.analysis.wb_shell_analysis import WBShellAnalysis
-from bmcs_shell.folding.analysis.wb_fe_triangular_mesh import WBShellFETriangularMesh
-from bmcs_shell.folding.analysis.fem.fe_triangular_mesh import FETriangularMesh
-from bmcs_shell.folding.analysis.fem.tri_xdomain_fe import TriXDomainFE
-from bmcs_shell.folding.analysis.fem.tri_xdomain_fe_mitc import TriXDomainMITC
-from bmcs_shell.folding.analysis.fets2d_mitc import FETS2DMITC
-from bmcs_shell.folding.analysis.fem.xdomain_fe_grid import XDomainFE
-from bmcs_shell.folding.analysis.wb_fets2d3u1m_fe import FETS2D3U1M
+# from bmcs_shell.folding.analysis.wb_shell_analysis import WBShellAnalysis
+# from bmcs_shell.folding.analysis.wb_fe_triangular_mesh import WBShellFETriangularMesh
+# from bmcs_shell.folding.analysis.fem.fe_triangular_mesh import FETriangularMesh
+# from bmcs_shell.folding.analysis.fem.tri_xdomain_fe import TriXDomainFE
+# from bmcs_shell.folding.analysis.fem.tri_xdomain_fe_mitc import TriXDomainMITC
+# from bmcs_shell.folding.analysis.fets2d_mitc import FETS2DMITC
+# from bmcs_shell.folding.analysis.fem.xdomain_fe_grid import XDomainFE
+# from bmcs_shell.folding.analysis.wb_fets2d3u1m_fe import FETS2D3U1M
+# from bmcs_shell.folding.analysis.fem.vmats2D_elastic import MATS2DElastic
+# from bmcs_shell.folding.analysis.fem.vmats_shell_elastic import MATSShellElastic
+# from bmcs_shell.folding.analysis.abaqus.abaqus_link_simple import AbaqusLink
+
 from bmcs_shell.folding.geometry.wb_cell.wb_cell import WBCell
 from bmcs_shell.folding.geometry.wb_cell.wb_cell_4p import WBCell4Param, WBCellSymb4Param
 from bmcs_shell.folding.geometry.wb_cell.wb_cell_4p_flat import WBCell4ParamFlat
@@ -25,8 +29,6 @@ from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_4p_flat import 
 from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_4p_ss import WBTessellation4PSS
 from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_4p_ex import WBTessellation4PEx
 from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_4p_ex_flat import WBTessellation4PExFlat
-from bmcs_shell.folding.analysis.fem.vmats2D_elastic import MATS2DElastic
-from bmcs_shell.folding.analysis.fem.vmats_shell_elastic import MATSShellElastic
 from bmcs_shell.folding.geometry.wb_tessellation.wb_num_tessellation_base import WBNumTessellationBase
 from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_base import WBTessellationBase
 from bmcs_shell.folding.geometry.wb_tessellation.wb_num_tessellation_invest import WBNumTessellationInvest
@@ -35,7 +37,6 @@ from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_5p_beta import 
 from bmcs_shell.folding.geometry.wb_tessellation.wb_tessellation_5p_vw import WBTessellation5PVW
 from bmcs_shell.folding.geometry.wb_tessellation.wb_num_tessellation_grad import WBNumTessellationGrad
 from bmcs_shell.folding.geometry.wb_tessellation.wb_num_tessellation_grad_invest import WBNumTessellationGradInvest
-from bmcs_shell.folding.analysis.abaqus.abaqus_link_simple import AbaqusLink
 from bmcs_shell.folding.geometry.wb_param_designer import WbParamDesigner
 from bmcs_shell.folding.geometry.wb_geo_utils import WBGeoUtils
 from bmcs_shell.folding.utils.dihedral_angles import get_dih_angles
